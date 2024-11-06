77f53ed0158ed03cb71a5092cbde6bfd9c774606
Sho Kiami <kiami.sho@gmail.com>
2024-11-06 00:32:44 -0800

    opt

--- solve.py
+++ solve.py
@@ -2,8 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from collections import deque
 from copy import deepcopy
-import sympy as sp
-import math
 
 EPS = 1e-5
 
@@ -14,7 +12,7 @@ class state:
     self.points = [(0.0, 0.0), (1.0, 0.0)]
 
 def goal_state():
-  n = 6
+  n = 3
   goal = state()
   theta = 2 * np.pi / n
   # for i in range(n):
@@ -33,6 +31,7 @@ def bfs(start, goal):
   visited = [start]
   while queue:
     state = queue.popleft()
+    # viz(state)
     print(len(state.circles) + len(state.lines))
     if done(state, goal):
       return state
@@ -109,7 +108,7 @@ def union(s1, s2):
 
 def line_eq(x1, y1, x2, y2):
   a, b, c = y2 - y1, x1 - x2, x2 * y1 - x1 * y2
-  d = math.sqrt(a**2 + b**2)
+  d = np.sqrt(a**2 + b**2)
   a /= d
   b /= d
   c /= d
@@ -120,7 +119,7 @@ def line_eq(x1, y1, x2, y2):
   return a, b, c
 
 def circle_eq(x1, y1, x2, y2):
-  return x1, y1, math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+  return x1, y1, np.sqrt((x2 - x1)**2 + (y2 - y1)**2)
 
 def equal(a, b):
   a = np.asarray(a)
@@ -132,31 +131,72 @@ def equal(a, b):
 def l_l_sol(line1, line2):
   a1, b1, c1 = line1
   a2, b2, c2 = line2
-  x, y = sp.symbols('x y', real=True)
-  eq1 = a1 * x + b1 * y + c1
-  eq2 = a2 * x + b2 * y + c2
-  sol = sp.solve([eq1, eq2], (x, y))
-  if len(sol) == 0:
+  if equal(a1 * b2 - a2 * b1, 0.0):
     return []
-  return [(sol[x], sol[y])]
+  if b1 != 0.0:
+    x = (b1 * c2 - b2 * c1) / (a1 * b2 - a2 * b1)
+    y = (c1 - a1 * x) / b1
+  else:
+    y = (a2 * c1 - a1 * c2) / (b2 * a1)
+    x = (c1 - b1 * y) / a1
+  return [(x, y)]
 
 def l_c_sol(line1, circle1):
   a1, b1, c1 = line1
   x1, y1, r1 = circle1
-  x, y = sp.symbols('x y', real=True)
-  eq1 = a1 * x + b1 * y + c1
-  eq2 = (x - x1)**2 + (y - y1)**2 - r1**2
-  sol = sp.solve([eq1, eq2], (x, y))
-  return sol
+  if b1 != 0:
+    A = 1 + (a1 / b1) ** 2
+    B = -2 * x1 + 2 * (a1 / b1) * (c1 / b1 + y1)
+    C = x1**2 + (c1 / b1 + y1)**2 - r1**2
+    D = B**2 - 4 * A * C
+    if D > EPS:
+      # Two intersection points
+      x1 = (-B + np.sqrt(D)) / (2 * A)
+      x2 = (-B - np.sqrt(D)) / (2 * A)
+      y1 = (-a1 * x1 - c1) / b1
+      y2 = (-a1 * x2 - c1) / b1
+      return [(x1, y1), (x2, y2)]
+    elif equal(D, 0.0):
+      x = -B / (2 * A)
+      y = (-a1 * x - c1) / b1
+      return [(x, y)]
+  else:
+    x = -c1 / a1
+    delta = r1**2 - (x - x1)**2
+    if delta > EPS:
+        y1 = y1 + np.sqrt(delta)
+        y2 = y1 - np.sqrt(delta)
+        return [(x, y1), (x, y2)]
+    elif equal(delta, 0.0):
+      y = y1
+      return [(x, y)]
+  return []
 
 def c_c_sol(circle1, circle2):
   x1, y1, r1 = circle1
   x2, y2, r2 = circle2
-  x, y = sp.symbols('x y', real=True)
-  eq1 = (x - x1)**2 + (y - y1)**2 - r1**2
-  eq2 = (x - x2)**2 + (y - y2)**2 - r2**2
-  sol = sp.solve([eq1, eq2], (x, y))
-  return sol
+  d = ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  if d > r1 + r2 + EPS or d < np.abs(r1 - r2) - EPS:
+    return []
+  elif equal(d, 0.0) and equal(r1, r2):
+    return []
+  elif equal(d, r1 + r2) or equal(d, np.abs(r1 - r2)):
+      a = (r1**2 - r2**2 + d**2) / (2 * d)
+      x = x1 + a * (x2 - x1) / d
+      y = y1 + a * (y2 - y1) / d
+      return [(x, y)]
+  a = (r1**2 - r2**2 + d**2) / (2 * d)
+  h = (r1**2 - a**2) ** 0.5
+  x = x1 + a * (x2 - x1) / d
+  y = y1 + a * (y2 - y1) / d
+
+  # Calculate the intersection points
+  x_res1 = x + h * (y2 - y1) / d
+  y_res1 = y - h * (x2 - x1) / d
+  x_res2 = x - h * (y2 - y1) / d
+  y_res2 = y + h * (x2 - x1) / d
+
+  return [(x_res1, y_res1), (x_res2, y_res2)]
 
 def viz(state):
   plt.figure()
@@ -168,7 +208,7 @@ def viz(state):
   plt.tight_layout()
 
   for a, b, c in state.lines:
-    d = math.sqrt(a**2 + b**2)
+    d = np.sqrt(a**2 + b**2)
     x1 = -a * c / d**2 + 1000 * b / d
     y1 = -b * c / d**2 - 1000 * a / d
     x2 = -a * c / d**2 - 1000 * b / d
