bde7b32a52c99b01e3212b39e472ed908d9cb54c
Sho Kiami <kiami.sho@gmail.com>
2025-12-29 23:51:02 -0800

    better viz

--- viz.py
+++ viz.py
@@ -1,23 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-BLACK = '#222222'
-GREY = '#aaaaaa'
-GOLD = '#f7b215'
+COLOR = {
+  'start': '#222222',
+  'state': '#888888',
+  'goal': '#f7b215'
+}
+
+PLOT_SIZE = {
+  'start': 15,
+  'state': 10,
+  'goal': 20
+}
+
+LINE_WIDTH = {
+  'start': 0.5,
+  'state': 1.5,
+  'goal': 2
+}
+
+ZORDER = {
+  'start': 1,
+  'state': 0,
+  'goal': 2
+}
 
 def parse(filename):
-  data = {
-    'start points': [],
-    'start lines': [],
-    'start circles': [],
-    'state points': [],
-    'state lines': [],
-    'state circles': [],
-    'goal points': [],
-    'goal lines': [],
-    'goal circles': [],
-    'goal segments': []
-  }
+  sections = [
+    'start points', 'start lines', 'start circles',
+    'state points', 'state lines', 'state circles',
+    'goal points',  'goal lines',  'goal circles', 'goal segments'
+  ]
+  data = {key: [] for key in sections}
   current = None
   with open(filename, 'r') as file:
     for line in file:
@@ -28,58 +42,50 @@ def parse(filename):
         current.append(list(map(float, line.split())))
   return data
 
-def line_endpoints(a, b, c):
+def line_endpoints(a, b, c, D=1000):
   d = np.sqrt(a**2 + b**2)
-  D = 1000
   x1 = a * c / d**2 + D * b / d
   y1 = b * c / d**2 - D * a / d
   x2 = a * c / d**2 - D * b / d
   y2 = b * c / d**2 + D * a / d
   return (x1, x2), (y1, y2)
 
-data = parse('temp.txt')
+def plot_points(points, color, size, zorder):
+  for x, y in points:
+    plt.scatter(x, y, color=color, s=size, zorder=zorder+len(ZORDER))
+    plt.scatter(x, y, color='#ffffff', s=3, zorder=2*len(ZORDER))
 
-plt.figure()
-plt.axis('equal')
-plt.axis([-4, 4, -4, 4])
-plt.axis('off')
-plt.box(False)
-plt.tight_layout()
+def plot_lines(lines, color, lw, zorder):
+  for a, b, c in lines:
+    x, y = line_endpoints(a, b, c)
+    plt.plot(x, y, color=color, lw=lw, zorder=zorder)
 
-for x, y in data['start points']:
-  plt.scatter(x, y, color=BLACK, s=5.1, zorder=1)
+def plot_circles(circles, color, lw, zorder):
+  for x, y, r2 in circles:
+    circle = plt.Circle((x, y), np.sqrt(r2), color=color, fill=False, lw=lw, zorder=zorder)
+    plt.gca().add_artist(circle)
 
-for a, b, c in data['start lines']:
-  x, y = line_endpoints(a, b, c)
-  plt.plot(x, y, color=BLACK, lw=1.1, zorder=1)
+def plot_segments(segments, color, lw, zorder):
+  for x1, y1, x2, y2 in segments:
+    plt.plot([x1, x2], [y1, y2], color=color, lw=lw, zorder=zorder)
 
-for x, y, r2 in data['start circles']:
-  circle = plt.Circle((x, y), np.sqrt(r2), color=BLACK, fill=False, lw=1.1, zorder=1)
-  plt.gca().add_artist(circle)
+def main():
+  data = parse('temp.txt')
 
-for x, y in data['state points']:
-  plt.scatter(x, y, color=GREY, s=5, zorder=0)
+  plt.figure()
+  plt.axis('equal')
+  plt.axis([-4, 4, -4, 4])
+  plt.axis('off')
+  plt.box(False)
+  plt.tight_layout()
 
-for a, b, c in data['state lines']:
-  x, y = line_endpoints(a, b, c)
-  plt.plot(x, y, color=GREY, lw=1, zorder=0)
+  for phase in ('start', 'state', 'goal'):
+    plot_points(data[f'{phase} points'], COLOR[phase], PLOT_SIZE[phase], zorder=ZORDER[phase])
+    plot_lines(data[f'{phase} lines'], COLOR[phase], LINE_WIDTH[phase], zorder=ZORDER[phase])
+    plot_circles(data[f'{phase} circles'], COLOR[phase], LINE_WIDTH[phase], zorder=ZORDER[phase])
+  plot_segments(data['goal segments'], COLOR['goal'], LINE_WIDTH['goal'], ZORDER['goal'])
 
-for x, y, r2 in data['state circles']:
-  circle = plt.Circle((x, y), np.sqrt(r2), color=GREY, fill=False, lw=1, zorder=0)
-  plt.gca().add_artist(circle)
+  plt.show()
 
-for x, y in data['goal points']:
-  plt.scatter(x, y, color=GOLD, s=5.2, zorder=2)
-
-for a, b, c in data['goal lines']:
-  x, y = line_endpoints(a, b, c)
-  plt.plot(x, y, color=GOLD, lw=1.2, zorder=2)
-
-for x, y, r2 in data['goal circles']:
-  circle = plt.Circle((x, y), np.sqrt(r2), color=GOLD, fill=False, lw=1.2, zorder=2)
-  plt.gca().add_artist(circle)
-
-for x1, y1, x2, y2 in data['goal segments']:
-  plt.plot([x1, x2], [y1, y2], color=GOLD, lw=1.2, zorder=2)
-
-plt.show()
+if __name__ == "__main__":
+  main()
